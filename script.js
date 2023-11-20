bef9a190d223551874118502326d2ea890dfcc30
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-21 00:39:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-        // Get the body element
-        var body = document.body;
-
-        // Check if the body has no children
-        if (body.childElementCount === 0) {
-            // Create a new paragraph element
-            var paragraph = document.createElement('p');
-
-            // Set the text content of the paragraph
-            paragraph.textContent = 'DOM load success';
-
-            // Append the paragraph to the body
-            body.appendChild(paragraph);
-        }
-    });
+document.addEventListener("DOMContentLoaded",function(){
+	var body=document.body;
+	body.innerHTML="DOM load success";
+});
